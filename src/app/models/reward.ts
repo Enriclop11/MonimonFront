1af835db5c060ae4b6eb96339a3d0a8f565adc04
Enriclop11 <informenric@gmail.com>
2025-06-19 12:31:45 +0200

    Rewards Price

--- src/app/models/reward.ts
+++ src/app/models/reward.ts
@@ -4,18 +4,21 @@ export class Reward {
   active: boolean;
   modOnly: boolean;
   cooldown: number;
+  price: number;
 
   constructor(
     name: string,
     reward: string,
     active: boolean,
     modOnly: boolean,
-    cooldown: number
+    cooldown: number,
+    price: number
   ) {
     this.name = name;
     this.reward = reward;
     this.active = active;
     this.modOnly = modOnly;
     this.cooldown = cooldown;
+    this.price = price;
   }
 }
